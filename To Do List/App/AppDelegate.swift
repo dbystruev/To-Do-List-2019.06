8a78a7757f89f70722c97e48794b9157048ffb44
Denis Bystruev <github@learnSwift.ru>
2019-06-21 22:21:19 +0300

    Cleaned App Delegate

---
+++ To Do List/App/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  To Do List
+//
+//  Created by Denis Bystruev on 21/06/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
